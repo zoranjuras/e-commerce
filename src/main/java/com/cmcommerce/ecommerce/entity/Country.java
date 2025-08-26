81d5867206841e2d5735a64281c34441f623939d
zoranjuras <cmundir@gmail.com>
2025-08-26 13:13:49 +0200

    Created JPA entities for countries and states

---
+++ src/main/java/com/cmcommerce/ecommerce/entity/Country.java
@@ -0,0 +1,27 @@
+package com.cmcommerce.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Data
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+
+}
